1a8e5ea1516c1dc269766c424c15836e32af2371
Ladmia Madi <ladmia.madi@gmail.com>
2022-02-14 21:16:29 +0100

    create login and home HTML pages

--- src/main/java/com/openclassrooms/payMyBuddy/PayMyBuddyApplication.java
+++ src/main/java/com/openclassrooms/payMyBuddy/PayMyBuddyApplication.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import java.math.BigDecimal;
 import java.text.DateFormat;
@@ -18,8 +19,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootApplication
-public class PayMyBuddyApplication implements CommandLineRunner {
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class PayMyBuddyApplication {
 
 	@Autowired
 	private UserService userService;
@@ -34,50 +35,4 @@ public class PayMyBuddyApplication implements CommandLineRunner {
 		SpringApplication.run(PayMyBuddyApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		/*User userToAdd = new User();
-
-		userToAdd.setEmail("lea.anais@gmail.com");
-		userToAdd.setFirstName("Anais");
-		userToAdd.setLastName("Madi");
-		userToAdd.setBankAccount("1985FF");
-		userToAdd.setBalance(BigDecimal.valueOf(100));
-
-		userToAdd.setBankAccountBalance(BigDecimal.valueOf(1000));
-		userToAdd.setPassword("anais2015");
-
-		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		Date date = new Date();
-
-		userToAdd.setRegistrationDate(dateFormat.format(date));
-
-		System.out.println(userToAdd.getRegistrationDate());
-
-		userService.addUser(userToAdd);*/
-
-		//Connection connection = new Connection();
-		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		Date date = new Date();
-
-		//connection.setDateAdded(dateFormat.format(date));
-		//connection.setUserId(1);
-		//connection.setAddedUserId(4);
-
-		//connectionService.addConnection(connection);
-		Transaction transaction = new Transaction();
-
-		transaction.setTransactionDate(date);
-		transaction.setAmount(BigDecimal.valueOf(100));
-		transaction.setType("Transfert");
-		transaction.setUserId(1);
-
-		transactionService.addTransaction(transaction);
-
-		Optional<User> user = userService.getUserById(1);
-
-		//System.out.println(user.get().getAddedUsers());
-		System.out.println(user.get().getTransactions());
-	}
 }
---
+++ src/main/java/com/openclassrooms/payMyBuddy/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.payMyBuddy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String signIn () {
+        return "home";
+    }
+}
